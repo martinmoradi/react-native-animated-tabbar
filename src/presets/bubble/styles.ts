23f28f44131175de6efae5b4e52e7197f4b39eeb
Martin Moradi <61637044+martinmoradi@users.noreply.github.com>
2021-03-26 17:33:17 +0100

    Update styles.ts

--- src/presets/bubble/styles.ts
+++ src/presets/bubble/styles.ts
@@ -1,9 +1,11 @@
 import { StyleSheet } from 'react-native';
+import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
 
 export const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white',
     justifyContent: 'center',
     paddingHorizontal: 24,
+    marginHorizontal: wp('-2%)
   },
 });
